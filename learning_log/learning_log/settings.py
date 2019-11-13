5c95e48364ea332c383b1cae22e0b06101ce7942
曹力升 <954382491@qq.com>
2019-11-13 14:52:35 +0800

    ch20-1

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 第三方应用程序
+    'bootstrap3',
+
     # 我的应用程序
     'learning_logs',
     'users',
@@ -122,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# django-bootstrap3的设置
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
 
 
 # 我的设置
