69d5ce8117b2716d19d713ba59f255856cbef30b
YouXin <954382491@qq.com>
2020-11-05 16:55:18 +0800

    整理

---
+++ Book-Algorithms/subsetsum/fast.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# coding=utf-8
+
+# 
+# def FastSubsetSum(arr, sum):
+#     arrLen = len(arr)
+#     S = []
+#     for i in range(arrLen + 1):
+#         S.append(dict(t0=True))
+
+#     i = arrLen - 1
+#     while i >= 0:
+#         S[i][] = S[i+1][] or S[i+1][]
+#         i -= 1
+
+#     return S[0]['t{}'.format(sum)]
